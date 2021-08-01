76d4c4d6cad5e338153cf4e74d8b0583c5627717
studentFks2023 <fks2023@autuni.ac.nz>
2021-08-01 13:17:13 +1200

    Replaced all code in App.js with different code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    }
+  }
 
-      <h1>Welcome to CISE - the home of learning and fun</h1>
+  makeIncrementer = amount => () =>
+    this.setState(prevState => ({
+      count: prevState.count + amount,
+    }));
 
-    </div>
-  );
+  increment = this.makeIncrementer(1);
+
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button className="increment" onClick={this.increment}>Increment count</button>
+      </div>
+    )
+  }
 }
 
 export default App;
